d78930007f2355f8e117ca604e75b89a9e68a919
Mehakpreet Kaur <mehakpreetkaur3@academic.rrc.ca>
2024-05-03 11:36:15 -0500

    function expression

--- chapter4_sandbox.js
+++ chapter4_sandbox.js
@@ -3,6 +3,11 @@ function greet(){
     console.log('hello there');
 }
 
-//function expression
+//function expression, semicolon at the end
+const speak = function(){
+    console.log('good day!')
+};
 
 greet();//calling or invoking the function
+
+speak();
