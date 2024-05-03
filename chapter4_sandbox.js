f465e932c615f9a730cab7bc9d254e687dc865f8
Mehakpreet Kaur <mehakpreetkaur3@academic.rrc.ca>
2024-05-03 13:33:58 -0500

    arrow functions

--- chapter4_sandbox.js
+++ chapter4_sandbox.js
@@ -24,12 +24,33 @@ const spoke = function(name='sham',time='night'){
 spoke('mario', 'morning');
 
 //returning values
-const calcArea = function(radius){
-    let area = 3.14 * radius**2;
-    return area;
+// const calcArea = function(radius){
+//     let area = 3.14 * radius**2;
+//     return area;
+// };
+
+//arrow function
+const calcArea = (radius) =>{
+    return 3.14 * radius**2;
 };
 
 const area = calcArea(5);
-console.log(area);
+console.log('area is:' ,area);
 
 
+// const bill = function(products,tax){
+//     let total = 0;
+//     for(let i=0; i<products.length; i++){
+//         total += products[i] +products[i] *tax;
+//     }
+//     return total;
+// }
+
+const bill = (products,tax) =>{
+    let total = 0;
+    for(let i=0; i<products.length; i++){
+        total += products[i] +products[i] *tax;
+    }
+    return total; 
+}
+console.log(bill([10,15,30], 0.2));
