d0e043ff1d31482c756548a91ce8b1497a668f69
Mehakpreet Kaur <mehakpreetkaur3@academic.rrc.ca>
2024-05-03 11:06:17 -0500

    Switch statements

--- sandbox_chapter3.js
+++ sandbox_chapter3.js
@@ -94,3 +94,26 @@ for(let i= 0; i<scores.length; i++){
         break;
     }
 }
+
+//switch statements
+const grade = 'P';
+
+switch(grade){
+    case 'A': 
+        console.log('you got an A!');
+        break;
+    case 'B': 
+        console.log('you got an B!');
+        break;
+    case 'C': 
+        console.log('you got an C!');
+        break;
+    case 'D': 
+        console.log('you got an D!');
+        break;
+    case 'E': 
+        console.log('you got an E!');
+        break;
+    default: 
+        console.log('not a valid grade');
+}
