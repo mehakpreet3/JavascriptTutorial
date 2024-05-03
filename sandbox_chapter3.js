40d5c28c416136efe63dcde2d743b59426d76742
Mehakpreet Kaur <mehakpreetkaur3@academic.rrc.ca>
2024-05-03 10:38:46 -0500

    break and continue

--- sandbox_chapter3.js
+++ sandbox_chapter3.js
@@ -78,3 +78,19 @@ if(!user){
 
 console.log(!true);
 console.log(!false)
+
+//break and continue
+const scores = [50,25,0,30,100,20,10];
+
+for(let i= 0; i<scores.length; i++){
+    console.log('your score: ', scores[i]);
+
+    if(scores[i===0]){
+        continue;
+    }
+
+    if(scores[i] === 100){
+        console.log('congrats, you got the the highest score!');
+        break;
+    }
+}
