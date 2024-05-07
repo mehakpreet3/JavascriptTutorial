9bd9515d563734df15c6723a9d670dbc8a27c7d8
Mehakpreet Kaur <mehakpreetkaur3@academic.rrc.ca>
2024-05-07 11:25:12 -0500

    get elements by ID,Class, TagName

--- Chapter6.js
+++ Chapter6.js
@@ -7,8 +7,24 @@
 
 // console.log(para);
 
-const paras = document.querySelectorAll('p');
+// const paras = document.querySelectorAll('p');
 
-const errors = document.querySelectorAll('.error');
+// const errors = document.querySelectorAll('.error');
 
-console.log(errors);
+// console.log(errors);
+
+//get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+//get elements by their class name
+
+// const errors =document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+//forEach cannot be used in html collection
+
+//get elements by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
