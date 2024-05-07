e6dfdb9dd663ad16329696b7c35fee5209b93e92
Mehakpreet Kaur <mehakpreetkaur3@academic.rrc.ca>
2024-05-07 08:02:46 -0500

    math objects

--- chapter5_sandbox.js
+++ chapter5_sandbox.js
@@ -1,23 +1,68 @@
 //object literals
+// const blogs =[
+//     {title: 'why mac and cheese rules', likes:30},
+//     {title: '10 things to make with marmite', likes: 50}
+// ];
+
+// console.log(blogs);
+
 
 let admin = {
     name: 'crystal',
     age :38,
     email: 'crstal@theninja.co.uk',
     location: 'berlin',
-    blog: ['why mac and cheese rules', '10 things to make with marmite']
+    blog: [ {title: 'why mac and cheese rules', likes:30},
+            {title: '10 things to make with marmite', likes: 50}
+],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout(){
+        console.log('the user logged out');
+    },
+    logBlogs(){
+        console.log('this user has written the following blogs:');
+        this.blog.forEach(blog =>{
+            console.log(blog.title, blog.likes);
+        })
+    }
 };
 
-console.log(admin);
-console.log(admin.name);
+admin.login();
+admin.logout();
+admin.logBlogs();
 
-admin.age = 35;
-console.log(admin.age);
+// console.log(admin);
+// console.log(admin.name);
+
+// admin.age = 35;
+// console.log(admin.age);
 
 //using square brackets we can access objects property
-console.log(admin['email']);
+// console.log(admin['email']);
 
-admin['name'] = 'mehak';
-console.log(admin['name']);
+// admin['name'] = 'mehak';
+// console.log(admin['name']);
 
-console.log(typeof admin);
+// console.log(typeof admin);
+
+
+//Math object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const areaOfCircle = 7.7;
+
+console.log(Math.round(areaOfCircle));
+console.log(Math.floor(areaOfCircle));
+console.log(Math.ceil(areaOfCircle));
+console.log(Math.trunc(areaOfCircle));
+
+//random numbers
+
+const random = Math.random();
+console.log(random);
+console.log (Math.round(random * 100));
