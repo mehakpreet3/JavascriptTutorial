f07c734202c95a37e765002de77cde0e98e7da57
Mehakpreet Kaur <mehakpreetkaur3@academic.rrc.ca>
2024-05-07 11:11:48 -0500

    querySelector &querySelectorAll

--- Chapter6.js
+++ Chapter6.js
@@ -3,3 +3,12 @@
 //add, change amd delete content
 //make a cool pop-up effect in web page
 
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+const paras = document.querySelectorAll('p');
+
+const errors = document.querySelectorAll('.error');
+
+console.log(errors);
